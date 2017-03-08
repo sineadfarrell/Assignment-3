91b9a291050e959bacfb5fdad67a4affc4b756ce
Murnane1 <conor.murnane@ucdconnect.ie>
2017-03-08 19:46:26 +0000

    Add files via upload

---
+++ slot.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void)
+{
+	int numSlots[20];
+	printf("Select a number of slots");
+	scanf("%d", &numSlots);
+	printf("%d", numSlots);
+	
+	
+	return 0;
+}
